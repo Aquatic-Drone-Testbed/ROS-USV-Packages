e7c7483d4d3190b0da7ab568c8716f9632def6f7
Maxwell Jung <jungmaxwell@gmail.com>
2023-11-28 23:14:50 +0000

    add turtle tutorial install commands

--- .devcontainer/env-setup.sh
+++ .devcontainer/env-setup.sh
@@ -2,3 +2,7 @@
 echo "source /opt/ros/iron/setup.bash" >> ~/.bashrc
 sudo rosdep update
 sudo rosdep install --from-paths src --ignore-src -y
+
+# Uncomment below lines to install ROS turtle tutorial after creating dev container
+# sudo apt update
+# sudo apt install -y ros-iron-turtlesim ~nros-iron-rqt*
