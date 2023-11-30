9b83a510a45e7193fdfc6f3222b33090c5475858
Maxwell Jung <jungmaxwell@gmail.com>
2023-11-30 00:53:06 +0000

    change ros distro from iron to humble

--- .devcontainer/env-setup.sh
+++ .devcontainer/env-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "source /opt/ros/iron/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 rosdep update
-rosdep install -i --from-path src --rosdistro iron -y
+rosdep install -i --from-path src --rosdistro humble -y
 
 # Uncomment below lines to install ROS turtle tutorial after creating dev container
 # sudo apt update
-# sudo apt install -y ros-iron-turtlesim ~nros-iron-rqt*
+# sudo apt install -y ros-humble-turtlesim ~nros-humble-rqt*
