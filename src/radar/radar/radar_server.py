1998ec7d8574ef4ecc12ad683941b126e10df631
Maxwell Jung <jungmaxwell@gmail.com>
2023-12-21 03:11:35 +0000

    keep server alive

--- src/radar/radar/radar_server.py
+++ src/radar/radar/radar_server.py
@@ -7,13 +7,14 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen(1)
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data: break
-                conn.sendall(data)
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data: break
+                    conn.sendall(data)
 
 
 if __name__ == '__main__':
