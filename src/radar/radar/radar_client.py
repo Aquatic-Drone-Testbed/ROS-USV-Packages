cfddecf18c3a0220ea47417814ba447a9eae8554
Maxwell Jung <jungmaxwell@gmail.com>
2023-12-21 03:01:07 +0000

    try sending byte array

--- src/radar/radar/radar_client.py
+++ src/radar/radar/radar_client.py
@@ -2,11 +2,15 @@
 import socket
 
 def main():
+    rd_msg_tx_control = bytes([0x10, 0x00, 0x28, 0x00,
+                               0x01,  # Control value at offset 4 : 0 - off, 1 - on
+                               0x00, 0x00, 0x00])
+    
     HOST = 'localhost'        # The remote host
     PORT = 50007              # The same port as used by the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b'Hello, world')
+        s.sendall(rd_msg_tx_control)
         data = s.recv(1024)
     print('Received', repr(data))
 
