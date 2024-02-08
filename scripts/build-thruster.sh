b4bd61fd74eec8f59cc274704c3e3c13c377152c
Dhruv Aggarwal <dna@ucsb.edu>
2024-02-08 14:07:50 -0800

    fixed script issue lol

--- scripts/build-thruster.sh
+++ scripts/build-thruster.sh
@@ -3,7 +3,7 @@
 sudo apt install wget unzip -y
 wget https://github.com/joan2937/pigpio/archive/master.zip
 unzip master.zip
-cd pigpio-mastersudo
+cd pigpio-master
 make
 sudo make install
 sudo apt-get install python-setuptools python3-setuptools python3-pigpio
