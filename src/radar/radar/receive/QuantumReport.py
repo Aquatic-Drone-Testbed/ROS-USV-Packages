5193a17593af4e4b0d0da4d6fcb78b54e96cf27f
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2024-02-21 08:09:09 +0000

    further sub-parse quantum report

--- src/radar/radar/receive/QuantumReport.py
+++ src/radar/radar/receive/QuantumReport.py
@@ -23,4 +23,12 @@ class QuantumReport:
     
     @staticmethod
     def parse_report(data: bytes):
-        return struct.unpack('<IB9sHBB2sBB32sBB3sB88s80s32s', data)
+        fields = list(struct.unpack('<IB9sHBB2sBB32sBB3sB88s80s32s', data))
+        fields[2] = struct.unpack('<9B', fields[2])
+        fields[6] = struct.unpack('<2B', fields[6])
+        fields[9] = list(struct.unpack('<8s8s8s8s', fields[9]))
+        fields[12] = struct.unpack('<3B', fields[12])
+        fields[14] = struct.unpack('<88B', fields[14])
+        fields[15] = struct.unpack('<20I', fields[15])
+        fields[16] = struct.unpack('<8I', fields[16])
+        return fields
