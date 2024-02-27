9accb1bdc3b85dc4a5d32a9590981486a80b6f1c
BaronLuisYoung <baronyoung2001@gmail.com>
2024-02-27 11:22:21 -0800

    Updated controller to receive sub data.

--- src/thruster_control/thruster_control/thruster.py
+++ src/thruster_control/thruster_control/thruster.py
@@ -3,55 +3,55 @@ import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
-from sensor_msgs.msg import Joy
 import pigpio
 import time
 
 class ThrusterControl(Node):
+
+    #CONSTANTS FOR GPIO/ESC/THRUSTERS
+    ESC_START_UP_VAL = 1500
+    ESC_MAX_VAL = 1900
+    ESC_MIN_VAL = 1100
+    
+    GPIO_ESC_PIN1 = 12
+    GPIO_ESC_PIN2 = 13
+
     def __init__(self):
         super().__init__('thruster_control')
         
         self.subscription = self.create_subscription(
             String, 
             'thruster_control', 
-            self.listener_callback, 
+            self.listener_callback,  #receives messages/ processes controller input
             10)
-        self.subscription  # prevent unused variable warning
-        
-        self.pi = pigpio.pi() # connect to pi
+                    
+        self.pi = pigpio.pi() # connect to pi gpio
         if not self.pi.connected:
             self.get_logger().error('Not connected to Raspberry Pi GPIO. Exiting...')
             rclpy.shutdown()
 
-        '''
-        ESC value range for thrusters 
-            max (forward): 1900 
-            min (reverse): 1100
-        
-        Joystick value range
-            min max    
-        
-        '''
-        
-        self.esc1_pulsewidth = 1500 # Initial startup for ESC 1
-        self.esc2_pulsewidth = 1500 # Initial startup for ESC 2
+        self.esc1_pulsewidth = ESC_START_UP_VAL 
+        self.esc2_pulsewidth = ESC_START_UP_VAL 
         
         time.sleep(0.7)
         
         self.esc1_axis_value = 0  # Gets and stores latest joystick position
         self.esc2_axis_value = 0
         
-        self.pi.set_servo_pulsewidth(12, self.esc1_pulsewidth)  # gpio pin is 12
-        self.pi.set_servo_pulsewidth(13, self.esc2_pulsewidth)  # gpio pin is 12
+        self.pi.set_servo_pulsewidth(GPIO_ESC_PIN1, self.esc1_pulsewidth)  # gpio pin is 12
+        self.pi.set_servo_pulsewidth(GPIO_ESC_PIN2, self.esc2_pulsewidth)  # gpio pin is 13
         
         self.timer = self.create_timer(0.1, self.timer_callback)  # Adjust PWM vaue at most every 0.1 seconds
         
         self.get_logger().info('Thruster control initialized.')
-
         print(f"{'Timestamp':<20} | {'ESC 1 Pulsewidth':<17} | {'ESC 2 Pulsewidth'}")
         print("-" * 60)  # Print a separator line
 
-    def joy_callback(self, msg):
+    def listener_callback(self, msg):
+        #TODO get data and adjust thrusters 
+            '''
+                use msg value to update new_pulsewidth
+            '''
         # Sets axis to be vertical joystick axis only
         self.esc1_axis_value = msg.axes[1]
         self.esc2_axis_value = msg.axes[4]
@@ -59,15 +59,15 @@ class ThrusterControl(Node):
     def timer_callback(self):
         # Use the stored axis value to adjust PWM value
         adjustment = self.esc1_axis_value * 10  # Smaller scale factor for smoother adjustment
-        new_pulsewidth = max(1100, min(1900, self.esc1_pulsewidth + adjustment))
+        new_pulsewidth = max(ESC_MIN_VAL, min(ESC_MAX_VAL, self.esc1_pulsewidth + adjustment))
         if new_pulsewidth != self.esc1_pulsewidth: #updates PWM value if joystick moved
-            self.pi.set_servo_pulsewidth(12, new_pulsewidth)
+            self.pi.set_servo_pulsewidth(GPIO_ESC_PIN1, new_pulsewidth)
             self.esc1_pulsewidth = new_pulsewidth
 
         adjustment2 = self.esc2_axis_value * 10  # Smaller scale factor for smoother adjustment
-        new_pulsewidth2 = max(1100, min(1900, self.esc2_pulsewidth + adjustment2))
+        new_pulsewidth2 = max(ESC_MAX_VAL, min(ESC_MAX_VAL, self.esc2_pulsewidth + adjustment2))
         if new_pulsewidth != self.esc2_pulsewidth: #updates PWM value if joystick moved
-            self.pi.set_servo_pulsewidth(13, new_pulsewidth2)
+            self.pi.set_servo_pulsewidth(GPIO_ESC_PIN2, new_pulsewidth2)
             self.esc2_pulsewidth = new_pulsewidth2
         
         timestamp = time.strftime("%H:%M:%S", time.localtime())
@@ -80,8 +80,8 @@ def main(args=None):
     thruster_control = ThrusterControl()
     executor = MultiThreadedExecutor()
     rclpy.spin(thruster_control, executor=executor)
-    thruster_control.pi.set_servo_pulsewidth(12, 1500)  # Reset to neutral on shutdown
-    thruster_control.pi.set_servo_pulsewidth(13, 1500)  # Reset to neutral on shutdown
+    thruster_control.pi.set_servo_pulsewidth(GPIO_ESC_PIN1, ESC_START_UP_VAL)  # Reset to neutral on shutdown
+    thruster_control.pi.set_servo_pulsewidth(GPIO_ESC_PIN2, ESC_START_UP_VAL)  # Reset to neutral on shutdown
     thruster_control.destroy_node()
     rclpy.shutdown()
 
