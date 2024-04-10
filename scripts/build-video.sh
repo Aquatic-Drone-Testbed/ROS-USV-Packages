86a08cc40f4e7d57188846b7e1030fc89cd9493d
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2024-04-10 10:29:11 -0700

    install cv-bridge

--- scripts/build-video.sh
+++ scripts/build-video.sh
@@ -3,7 +3,7 @@
 
 source install/setup.bash
 # sudo apt install libopencv-dev -y
-# sudo apt install ros-humble-cv-bridge -y
+sudo apt install ros-humble-cv-bridge -y
 pip install imageio-ffmpeg
 pip install imageio
 sudo apt-get update && sudo apt-get upgrade -y
