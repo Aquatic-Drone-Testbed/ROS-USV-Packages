84689aa61f8d43fab22ecd2c5e38a0b9abae8fb3
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2024-04-23 03:26:11 +0000

    include radar build in build all

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 # To gain permission: chmod +x scripts/build-all.sh
-chmod +x ./scripts/build-gps.sh
-chmod +x ./scripts/build-video.sh
-chmod +x ./scripts/rosdep.sh
-chmod +x ./scripts/build-thruster.sh
-chmod +x ./scripts/build-radio.sh
-chmod +x ./scripts/run-gps.sh
-chmod +x ./scripts/run-radio-receiver.sh
-chmod +x ./scripts/run-radio-sender.sh
-chmod +x ./scripts/run-thruster.sh
-chmod +x ./scripts/run-video.sh
+chmod +x ./scripts/*
 ./scripts/build-gps.sh
 ./scripts/build-video.sh
 ./scripts/rosdep.sh
 ./scripts/build-thruster.sh
 ./scripts/build-radio.sh
+./scripts/build-radar.sh
