2da16b309725aa65ad6389bb3929140d8f12f6d1
MarcuX009 <pk421933605@gmail.com>
2024-05-04 01:22:19 +0000

    WIP

--- src/robot_localization/launch/ekf.launch.py
+++ src/robot_localization/launch/ekf.launch.py
@@ -1,37 +1,27 @@
-# Copyright 2018 Open Source Robotics Foundation, Inc.
-# Copyright 2019 Samsung Research America
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
-import yaml
-from launch.substitutions import EnvironmentVariable
-import pathlib
-import launch.actions
-from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
+    config_ekf = os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')
+    config_navsat = os.path.join(get_package_share_directory("robot_localization"), 'params', 'navsat_transform.yaml')
+    
     return LaunchDescription([
         launch_ros.actions.Node(
             package='robot_localization',
             executable='ekf_node',
             name='ekf_filter_node',
             output='screen',
-            parameters=[os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')],
-           ),
+            parameters=[config_ekf],
+        ),
+        launch_ros.actions.Node(
+            package='robot_localization',
+            executable='navsat_transform_node',
+            name='navsat_transform_node',
+            output='screen',
+            parameters=[config_navsat],
+        ),
         launch_ros.actions.Node(
             package='tf2_ros',
             executable='static_transform_publisher',
@@ -46,10 +36,25 @@ def generate_launch_description():
             arguments=['0', '0', '0', '0', '0', '0', '1', 'bno055', 'base_link'],
             output='screen'
         ),
-        launch_ros.actions.Node(
-            package='gps_driver',
-            executable='gps_node',
-            name='gps_driver',
+         launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='odom_to_gps',
+            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'gps'],
             output='screen'
         ),
-])
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='gps_to_base_link',
+            arguments=['0', '0', '0', '0', '0', '0', 'gps', 'base_link'],
+            output='screen'
+        ),
+        # launch_ros.actions.Node(
+        #     package='gps_driver',
+        #     executable='gps_node',
+        #     name='gps_driver',
+        #     output='screen'
+        # ),
+    ])
+
