f9a309abc92f68f7e038728e4e93da7f17fd268b
Maxwell Jung <jungmaxwell@gmail.com>
2024-05-09 19:41:14 +0000

    fix shell scripts

--- scripts/run-receiver.sh
+++ scripts/run-receiver.sh
@@ -2,6 +2,4 @@
 # To gain permission: chmod +x scripts/run-gps.sh
 source install/setup.bash
 echo "Running Radar Receiver node..."
-# ros2 run radar quantum --ros-args --log-level debug -p host:="193.168.1.2"
 ros2 run radar receiver --ros-args --log-level debug
-# ros2 run radar slam
