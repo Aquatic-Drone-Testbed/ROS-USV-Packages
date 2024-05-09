9b8e82b991f35e1ab2e7cccf6c6aa0622169d671
Maxwell Jung <40750924+MaxwellJung@users.noreply.github.com>
2024-05-09 12:37:21 -0700

    fix: set thruster to 0 on radio timeout (#50)
    * WIP
    
    * set thruster to 0 on timeout

--- src/thruster_control/thruster_control/thruster.py
+++ src/thruster_control/thruster_control/thruster.py
@@ -5,72 +5,73 @@ from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 import pigpio
 import time
+import numpy as np
 
 class ThrusterControl(Node):
-
     #CONSTANTS FOR GPIO/ESC/THRUSTERS
     ESC_START_UP_VAL = 1500
     ESC_MAX_VAL = 1850
     ESC_MIN_VAL = 1150
-    
+    ESC_MAGNITUE = ESC_MAX_VAL - ESC_START_UP_VAL
+
     GPIO_ESC_PIN1 = 12
     GPIO_ESC_PIN2 = 13
 
     def __init__(self):
         super().__init__('thruster_control')
-        self.base_thrust = 0
-        self.delta_thrust = 0
-
         self.subscription = self.create_subscription(
             String, 
             'thruster_control', 
             self.listener_callback,  #receives messages/ processes controller input
             10)
-        
+
         self.pi = pigpio.pi() # connect to pi gpio
         if not self.pi.connected:
             self.get_logger().error('Not connected to Raspberry Pi GPIO. Exiting...')
             rclpy.shutdown()
-        
-        self.leftESC_pulsewidth = ThrusterControl.ESC_START_UP_VAL 
-        self.rightESC_pulsewidth = ThrusterControl.ESC_START_UP_VAL 
-        
+
+        self.base_thrust = 0
+        self.delta_thrust = 0
+
         time.sleep(0.7)
-        
-        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN1, self.leftESC_pulsewidth)  # gpio pin is 12
-        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN2, self.rightESC_pulsewidth)  # gpio pin is 13
         
         self.timer = self.create_timer(0.01, self.timer_callback)  # Adjust PWM vaue at most every 0.1 seconds
         
         self.get_logger().info('Thruster control initialized.')
-        print(f"{'Timestamp':<20} | {'Left ESC Pulsewidth':<17} | {'Right ESC Pulsewidth'}")
-        print("-" * 60)  # Print a separator line
-
+        self.get_logger().info(f"{'Timestamp':<20} | {'Left ESC Pulsewidth':<17} | {'Right ESC Pulsewidth'}")
+        self.get_logger().info("-" * 60)  # Print a separator line
 
 
     def listener_callback(self, msg):
+        if msg.data == "RADIO_TIMEOUT":
+            self.base_thrust = 0
+            self.delta_thrust = 0
+            return
+        
         #Adjust thruster values
-        data_parts = msg.data.split(',')
-        if data_parts[0] == "ABS_Y":
-            self.base_thrust = round(int(data_parts[1]))/350 # -1 to 1
-            
-        if data_parts[0] == "ABS_X":    
-            self.delta_thrust = round(int(data_parts[1]))/350 # -1 to 1
+        direction, value_str = msg.data.split(',')
+        value = round(int(value_str))/ThrusterControl.ESC_MAGNITUE # -1 to 1
+        
+        if direction == "ABS_Y":
+            self.base_thrust = value
+        elif direction == "ABS_X":
+            self.delta_thrust = value
 
-        leftVal = self.ESC_START_UP_VAL + 350 * (self.base_thrust + self.delta_thrust)
-        rightVal = self.ESC_START_UP_VAL + 350 * (self.base_thrust - self.delta_thrust)
-        
-        self.leftESC_pulsewidth = max(self.ESC_MIN_VAL, min(self.ESC_MAX_VAL, leftVal))
-        self.rightESC_pulsewidth = max(self.ESC_MIN_VAL, min(self.ESC_MAX_VAL, rightVal))
-        
+
     def timer_callback(self):
+        left_val = self.ESC_START_UP_VAL + ThrusterControl.ESC_MAGNITUE * (self.base_thrust + self.delta_thrust)
+        right_val = self.ESC_START_UP_VAL + ThrusterControl.ESC_MAGNITUE * (self.base_thrust - self.delta_thrust)
+
+        # clamp between min and max value
+        left_val = np.clip(left_val, self.ESC_MIN_VAL, self.ESC_MAX_VAL)
+        right_val = np.clip(right_val, self.ESC_MIN_VAL, self.ESC_MAX_VAL)
+
         # Set GPIO PWM values
-        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN1, self.leftESC_pulsewidth)
-        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN2, self.rightESC_pulsewidth)
-        
+        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN1, left_val)
+        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN2, right_val)
+
         timestamp = time.strftime("%H:%M:%S", time.localtime())
-        # Print ESC values in table format
-        print(f"{timestamp:<20} | Left ESC: {self.leftESC_pulsewidth:<17} | Right ESC: {self.rightESC_pulsewidth}")
+        self.get_logger().info(f"{timestamp:<20} | Left ESC: {self.leftESC_pulsewidth:<17} | Right ESC: {self.rightESC_pulsewidth}")
 
 
 def main(args=None):
