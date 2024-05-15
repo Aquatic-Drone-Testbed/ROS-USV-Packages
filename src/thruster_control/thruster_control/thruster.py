b807b9e96444d12c32b5d2682fb72db265ff5c9a
Maxwell Jung <jungmaxwell@gmail.com>
2024-05-15 17:44:51 +0000

    re-flip thrusters

--- src/thruster_control/thruster_control/thruster.py
+++ src/thruster_control/thruster_control/thruster.py
@@ -5,9 +5,9 @@ from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 import pigpio
 import time
+import numpy as np
 
 class ThrusterControl(Node):
-
     #CONSTANTS FOR GPIO/ESC/THRUSTERS
     ESC_BASE_VAL = 1500
     ESC_MAX_VAL = 1850
@@ -19,27 +19,21 @@ class ThrusterControl(Node):
 
     def __init__(self):
         super().__init__('thruster_control')
-        self.base_thrust = 0
-        self.delta_thrust = 0
-
         self.subscription = self.create_subscription(
             String, 
             'thruster_control', 
             self.listener_callback,  #receives messages/ processes controller input
             10)
-        
+
         self.pi = pigpio.pi() # connect to pi gpio
         if not self.pi.connected:
             self.get_logger().error('Not connected to Raspberry Pi GPIO. Exiting...')
             rclpy.shutdown()
-        
-        self.leftESC_pulsewidth = ThrusterControl.ESC_START_UP_VAL 
-        self.rightESC_pulsewidth = ThrusterControl.ESC_START_UP_VAL 
-        
+
+        self.base_thrust = 0
+        self.delta_thrust = 0
+
         time.sleep(0.7)
-        
-        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN1, self.leftESC_pulsewidth)  # gpio pin is 12
-        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN2, self.rightESC_pulsewidth)  # gpio pin is 13
         
         self.timer = self.create_timer(0.01, self.timer_callback)  # Adjust PWM vaue at most every 0.1 seconds
         
@@ -59,22 +53,28 @@ class ThrusterControl(Node):
         direction, value_str = msg.data.split(',')
         value = int(value_str)/ThrusterControl.ESC_MAGNITUE # normalize to [-1, 1]
         
+        if direction == "ABS_Y":
+            self.base_thrust = value
+        elif direction == "ABS_X":
+            self.delta_thrust = value
+
+
     def timer_callback(self):
         self.update_thrusters(self.base_thrust, self.delta_thrust)
 
 
     def update_thrusters(self, base, delta):
-        left_val = ThrusterControl.ESC_BASE_VAL - ThrusterControl.ESC_MAGNITUE * (base + delta)
-        right_val = ThrusterControl.ESC_BASE_VAL - ThrusterControl.ESC_MAGNITUE * (base - delta)
+        left_val = ThrusterControl.ESC_BASE_VAL + ThrusterControl.ESC_MAGNITUE * (base + delta)
+        right_val = ThrusterControl.ESC_BASE_VAL + ThrusterControl.ESC_MAGNITUE * (base - delta)
 
         # clamp between min and max value
         left_val = np.clip(round(left_val), ThrusterControl.ESC_MIN_VAL, ThrusterControl.ESC_MAX_VAL)
         right_val = np.clip(round(right_val), ThrusterControl.ESC_MIN_VAL, ThrusterControl.ESC_MAX_VAL)
 
         # Set GPIO PWM values
-        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN1, self.leftESC_pulsewidth)
-        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN2, self.rightESC_pulsewidth)
-        
+        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN1, left_val)
+        self.pi.set_servo_pulsewidth(ThrusterControl.GPIO_ESC_PIN2, right_val)
+
         timestamp = time.strftime("%H:%M:%S", time.localtime())
         self.get_logger().info(f"{timestamp:<10} | {f'Left ESC: {left_val}':<17} | Right ESC: {right_val}")
 
