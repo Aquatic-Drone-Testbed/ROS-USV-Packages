0bc1604e5993c309066f66cd00384eb40e98fb97
Maxwell Jung <jungmaxwell@gmail.com>
2024-05-15 00:24:39 +0000

    build: do not run env-setup script

--- .devcontainer/env-setup.sh
+++ .devcontainer/env-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "source /opt/ros/iron/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 sudo rosdep update
 sudo rosdep install --from-paths src --ignore-src -y
 
