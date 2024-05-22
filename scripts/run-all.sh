03520a775867fe23d70a02f4e5de0da779e331ad
Maxwell Jung <jungmaxwell@gmail.com>
2024-05-22 23:44:18 +0000

    script to run/kill all nodes

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -2,15 +2,17 @@
 
 export RCUTILS_LOGGING_USE_STDOUT=1
 
-# ./scripts/run-quantum.sh &
-# ./scripts/run-thruster.sh &
-# ./scripts/run-radio-receiver.sh &
+./scripts/run-quantum.sh &
 
-# ./scripts/run-imu.sh &
-# ./scripts/run-gps.sh &
-# ./scripts/run-ekf.sh &
+./scripts/run-thruster.sh &
+./scripts/run-radio-receiver.sh &
 
-# ./scripts/run-radio-sender.sh &
-# ./scripts/run-video.sh &
+./scripts/run-imu.sh &
+./scripts/run-gps.sh &
+./scripts/run-ekf.sh &
+
+./scripts/run-radio-sender.sh &
+./scripts/run-video.sh &
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+sleep infinity
